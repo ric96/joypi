6de5674bd237b2dc3bfae941d889f05224c42b4b
Sahaj Sarup <sahajsarup@gmail.com>
2015-06-17 16:16:29 +0530

    Create ir.py

---
+++ ir.py
@@ -0,0 +1,28 @@
+import time
+import signal
+import pygame
+import sys
+import RPi.GPIO as GPIO
+# The following is an example code written to controll the l298n motor contoller
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.IN) #Front Left
+GPIO.setup(8, GPIO.IN) #Front Center
+GPIO.setup(10, GPIO.IN) #Front Right
+GPIO.setup(23, GPIO.IN) #Back Left
+GPIO.setup(24, GPIO.IN) #Back Center
+GPIO.setup(26, GPIO.IN) #Back Right
+
+while True:
+  fl = GPIO.input(7)
+  print fl
+  fc = GPIO.input(8)
+  print fc
+  fr = GPIO.input(10)
+  print fr
+  bl = GPIO.input(23)
+  print bl
+  bc = GPIO.input(24)
+  print bc
+  br = GPIO.input(26)
+  print br
+  
