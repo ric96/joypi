d6aed1d272b82d2b43029e60ac6684a72e1a6438
Sahaj Sarup <sahajsarup@gmail.com>
2015-07-10 12:39:44 +0530

    Update camera.py

--- scripts/camera.py
+++ scripts/camera.py
@@ -11,7 +11,7 @@ try:
             sleep(0.5)
             if flag == 0:
               print "Sending Stream"  
-              os.system("raspivid -n -t 0 -w 1280 -h 720 -hf -ih -fps 20 -o - | nc -k -l 2222 &")
+              os.system("raspivid -n -t 0 -w 1280 -h 720 -hf -vf -ih -fps 20 -o - | nc -k -l 2222 &")
               flag = 1
             
             elif flag == 1:
