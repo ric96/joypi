929a045a150b64f63f968d698f63674e9db855fb
Sahaj Sarup <sahajsarup@gmail.com>
2015-07-10 11:46:27 +0530

    Update button.py

--- scripts/button.py
+++ scripts/button.py
@@ -1,12 +1,12 @@
 import RPi.GPIO as GPIO
 from time import sleep
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(31, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(32, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 try:  
     while True:            # this will carry on until you hit CTRL+C  
-        if GPIO.input(6): # if port 25 == 1  
+        if GPIO.input(31): # if port 25 == 1  
             print "Port 31 is 1/HIGH/True - LED ON"  
             #GPIO.output(24, 1)         # set port/pin value to 1/HIGH/True  
         #else:  
