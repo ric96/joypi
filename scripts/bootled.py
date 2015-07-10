52d1f1aebf9f1a8a4b4a8b67271df6af692fbd99
Sahaj Sarup <sahajsarup@gmail.com>
2015-07-10 12:07:00 +0530

    Update bootled.py

--- scripts/bootled.py
+++ scripts/bootled.py
@@ -1,8 +1,17 @@
 import RPi.GPIO as GPIO
 import os
-
+from time import sleep
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(13, GPIO.OUT)
-
-
+GPIO.setup(31, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.output(13, True)
+try:  
+    while True:
+        if GPIO.input(31) != 1:  
+            sleep(0.5)
+            print "Shutting down"  
+            os.system("halt")
+        sleep(0.1)
+  
+finally:             
+    GPIO.cleanup() 
