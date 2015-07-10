3130f8308c62d3ca0253f2efd0d602d89e967823
Sahaj Sarup <sahajsarup@gmail.com>
2015-07-10 12:01:17 +0530

    Update button.py

--- scripts/button.py
+++ scripts/button.py
@@ -7,12 +7,13 @@ GPIO.setup(32, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 try:  
     while True:            # this will carry on until you hit CTRL+C  
         if GPIO.input(31) != 1: # if port 25 == 1  
+            sleep(0.5)
             print "Port 31 is 1/HIGH/True - LED ON"  
             #GPIO.output(24, 1)         # set port/pin value to 1/HIGH/True  
         #else:  
          #   print "Port 32 is 0/LOW/False - LED OFF"  
             #GPIO.output(24, 0)         # set port/pin value to 0/LOW/False  
-        sleep(0.5)         # wait 0.1 seconds  
+        sleep(0.05)         # wait 0.1 seconds  
   
 finally:                   # this block will run no matter how the try block exits  
     GPIO.cleanup() 
